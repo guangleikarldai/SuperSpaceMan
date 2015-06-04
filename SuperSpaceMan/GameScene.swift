729f0282e91ab4192f3d3d562d66527be605baab
Guanglei Dai <guanglei.dai@fansentertainment.com>
2015-06-03 21:51:51 -0400

    Added Collision handling

--- SuperSpaceMan/GameScene.swift
+++ SuperSpaceMan/GameScene.swift
@@ -8,23 +8,64 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+struct GlobalConstants {
+    static let POWER_UP_NAME = "POWER_UP_ORB"
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     var backgroundNode : SKSpriteNode?
     var playerNode : SKSpriteNode?
+    var orbNode : SKSpriteNode?
     
+    let CollisionCategoryPlayer      : UInt32 = 0x1 << 1
+    let CollisionCategoryPowerUpOrbs : UInt32 = 0x1 << 2
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
+
     override init(size: CGSize) {
         super.init(size: size)
+        physicsWorld.gravity = CGVectorMake(0.0, -2.0);
+        physicsWorld.contactDelegate = self
+
+        userInteractionEnabled = true
         backgroundNode = SKSpriteNode(imageNamed: "Background")
         backgroundNode!.anchorPoint = CGPoint(x: 0.5, y: 0.0)
         backgroundNode!.position = CGPoint(x: size.width / 2.0, y: 0.0)
         addChild(backgroundNode!)
 
         playerNode = SKSpriteNode(imageNamed: "Player")
+        playerNode!.physicsBody = SKPhysicsBody(circleOfRadius: playerNode!.size.width / 2.0)
+        playerNode!.physicsBody!.dynamic = true
+
         playerNode!.position = CGPoint(x: size.width / 2.0, y: 80.0)
+        playerNode!.physicsBody!.linearDamping = 1.0
+        playerNode!.physicsBody!.allowsRotation = false
+        playerNode!.physicsBody!.categoryBitMask = CollisionCategoryPlayer
+        playerNode!.physicsBody!.contactTestBitMask = CollisionCategoryPowerUpOrbs
+        playerNode!.physicsBody!.collisionBitMask = 0
         addChild(playerNode!)
+
+        orbNode = SKSpriteNode(imageNamed: "PowerUp")
+        orbNode!.position = CGPoint(x: 150.0, y: size.height - 25)
+        orbNode!.physicsBody = SKPhysicsBody(circleOfRadius: orbNode!.size.width / 2.0)
+        orbNode!.physicsBody!.dynamic = false
+        orbNode!.physicsBody!.categoryBitMask = CollisionCategoryPowerUpOrbs
+        orbNode!.physicsBody!.collisionBitMask = 0
+        orbNode!.name = GlobalConstants.POWER_UP_NAME
+        addChild(orbNode!)
+    }
+
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        playerNode!.physicsBody!.applyImpulse(CGVectorMake(0.0, 40.0))
+    }
+
+    func didBeginContact(contact: SKPhysicsContact) {
+        var nodeB = contact.bodyB!.node!
+
+        if nodeB.name == GlobalConstants.POWER_UP_NAME {
+            nodeB.removeFromParent()
+        }
     }
 }
